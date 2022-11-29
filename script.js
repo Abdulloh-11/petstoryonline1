8ef91ef4a9b4e6801c551c818a404056f9183135
Abdulloh-11 <abdullohgulomov1234@gmail.com>
2022-11-29 23:00:48 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+ 
+        tailwind.config = {
+            theme: {
+                
+                extend: {
+                    container: {
+                        center: true,
+                      
+                        screens: {
+                            xsm: "370px",
+                            sm: "440px",
+                            md: "560px",
+                            lg: "768px",
+                            xl: "960px",
+                            "2xl": "1130px"
+                        }
+                    },
+
+                    backgroundImage: {
+                        backgroundImage: {
+                            
+                            
+                          }
+                      }
+                },
+                screens: {
+                    xsm: "370px",
+                    sm: "440px",
+                    md: "560px",
+                    lg: "768px",
+                    xl: "960px",
+                    "2xl": "1130px",
+                    "3xl": "1440px"
+                },
+                // screens: {
+                //     "3xl": { max: "1440px" },
+                //     "2xl": { max: "1130px" },
+                //     xl: { max: "960px" },
+                //     lg: { max: "768px" },
+                //     md: { max: "560px" },
+                //     sm: { max: "440px" },
+                //     xsm: { max: "370px" }
+                // },
+               
+
+                
+
+            }
+        }
